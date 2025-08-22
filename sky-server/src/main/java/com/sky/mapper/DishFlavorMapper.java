6ca694d37a516ba69baa883f518eeb70cb7f7db6
LongQianChen <1836448651@qq.com>
2025-08-22 14:22:16 +0800

    修改菜品业务代码开发

--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -4,6 +4,7 @@ import com.sky.entity.DishFlavor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -15,10 +16,24 @@ public interface DishFlavorMapper {
      */
     void insertBatch(List<DishFlavor> flavors);
     /**
-     * 根据菜品id查询口味数据
+     * 根据菜品id集合查询口味数据
      * @param dishId
      * @return
      */
     @Delete("delete from dish_flavor where dish_id = #{dishId}")
     void deleteByDishId(Long dishId);
+
+    /**
+     * 根据菜品id集合批量删除口味数据
+     * @param dishIds
+     */
+    void deleteByDishIds(List<Long> dishIds);
+
+    /**
+     * 根据菜品id查询口味数据
+     * @param dishId
+     * @return
+     */
+    @Select("select * from dish_flavor where dish_id = #{dishId}")
+    List<DishFlavor> getByDishId(Long dishId);
 }
